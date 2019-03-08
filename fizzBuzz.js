6fed2b12507aa612c5cec62ac0d4c9d586ac98e3
bweegee <bw33g33@gmail.com>
2019-03-08 15:35:50 -0700

    fizzbuzz updated

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,9 +1,10 @@
 const fizzBuzz = function() {
+	// fizz buzz values can be set to anything
 	let fizz = 3
 	let buzz = 5
 
 	for (let n = 1; n <= 100; n++) {
-		
+
 		if ((n % fizz === 0) && (n % buzz === 0))
 			console.log("FizzBuzz");
 		else if (n % fizz === 0)
