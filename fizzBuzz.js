4966bc8b8b01e8ab12a5dfe74752d9f4898eccf2
bweegee <bw33g33@gmail.com>
2019-03-08 15:30:58 -0700

    fizzbuzz added

---
+++ fizzBuzz.js
@@ -0,0 +1,18 @@
+const fizzBuzz = function() {
+	let fizz = 3
+	let buzz = 5
+
+	for (let n = 1; n <= 100; n++) {
+		
+		if ((n % fizz === 0) && (n % buzz === 0))
+			console.log("FizzBuzz");
+		else if (n % fizz === 0)
+			console.log("fizz");
+		else if (n % buzz === 0)
+			console.log("buzz");
+		else
+			console.log(n);
+	}
+}
+
+fizzBuzz();
