6ee72d8d5601aab8639891a1f6988ac9d6c72d47
bweegee <bw33g33@gmail.com>
2019-05-20 12:55:29 -0600

    refactored for speed

--- lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring.js
@@ -1,10 +1,10 @@
 // Given a string, find the length of the longest substring without repeating characters.
 
 /*
-  Time exceeded on "znwxtliqfklileyywbihmhtanyweb" 820ms
-  output was correct
-  ***refactor needed
-*/
+ * time exceeded on test 987/987
+ * string.length = 31000
+ * more optimization needed
+ */
 
 /**
  * @param {string} s
@@ -13,18 +13,53 @@
 var lengthOfLongestSubstring = function(s) {
   let currString = []
   let longest = 0
+
+  if (!s)
+    return 0
+
+  for (let i = 0; i <= s.length; i++) {
+    for (let l of s.substring(i)) {
+      if (currString.includes(l)) {
+          if (currString.length > longest)
+              longest = currString.length
+          currString.length = 0
+          currString.push(l)
+      } else {
+          currString.push(l)
+      }
+    }
+    if (longest < currString.length)
+      longest = currString.length
+    currString.length = 0
+  }
+
+   return longest
+}
+
+// Original recursive solution
+/*
+  Time exceeded on test case "znwxtliqfklileyywbihmhtanyweb" 820ms
+  output was correct
+*/
+
+/*
+var lengthOfLongestSubstring = function(s) {
+  let currString = []
+  let longest = 0
     
   if (!s)
     return 0
 
-  for (let l of s) {
-    if (currString.includes(l)) {
-        if (currString.length > longest)
-            longest = currString.length
-        currString.length = 0
-        currString.push(l)
-    } else {
-        currString.push(l)
+  for (let i = 0; i <= s.length; i++) {
+    for (let l of s.substring(i)) {
+      if (currString.includes(l)) {
+          if (currString.length > longest)
+              longest = currString.length
+          currString.length = 0
+          currString.push(l)
+      } else {
+          currString.push(l)
+      }
     }
   }
   
@@ -35,3 +70,4 @@ var lengthOfLongestSubstring = function(s) {
          : lengthOfLongestSubstring(s.substring(1))
 
 };
+*/
