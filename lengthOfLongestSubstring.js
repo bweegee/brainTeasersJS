bfe1673eff1180982721daee23307a93e5754ef1
pedro <pedro@flagship.cc>
2022-07-16 14:03:06 +0900

    longest substring using sliding window algorithm

--- lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring.js
@@ -1,73 +1,22 @@
-// Given a string, find the length of the longest substring without repeating characters.
-
-/*
- * time exceeded on test 987/987
- * string.length = 31000
- * more optimization needed
- */
-
 /**
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-  let currString = []
-  let longest = 0
+const lengthOfLongestSubstring = (s) => {
+  let max = 0
+  let end = 0
+  const set = new Set()
 
-  if (!s)
-    return 0
-
-  for (let i = 0; i <= s.length; i++) {
-    for (let l of s.substring(i)) {
-      if (currString.includes(l)) {
-          if (currString.length > longest)
-              longest = currString.length
-          currString.length = 0
-          currString.push(l)
-      } else {
-          currString.push(l)
+  for (let i = 0; i < s.length; i++) {
+    if (set.has(s[i])) {
+      while (set.has(s[i])) {
+        set.delete(s[end])
+        end++
       }
     }
-    if (longest < currString.length)
-      longest = currString.length
-    currString.length = 0
+    set.add(s[i])
+    const length = i - end + 1
+    max = Math.max(max, length)
   }
-
-   return longest
+  return max
 }
-
-// Original recursive solution
-/*
-  Time exceeded on test case "znwxtliqfklileyywbihmhtanyweb" 820ms
-  output was correct
-*/
-
-/*
-var lengthOfLongestSubstring = function(s) {
-  let currString = []
-  let longest = 0
-    
-  if (!s)
-    return 0
-
-  for (let i = 0; i <= s.length; i++) {
-    for (let l of s.substring(i)) {
-      if (currString.includes(l)) {
-          if (currString.length > longest)
-              longest = currString.length
-          currString.length = 0
-          currString.push(l)
-      } else {
-          currString.push(l)
-      }
-    }
-  }
-  
-  if (longest < currString.length)
-      longest = currString.length
-    
-  return longest > lengthOfLongestSubstring(s.substring(1)) ? longest
-         : lengthOfLongestSubstring(s.substring(1))
-
-};
-*/
