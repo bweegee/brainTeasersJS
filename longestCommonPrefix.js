1fb10848b73d105a4770585cc8d07f220f44429f
pedro <pedro@flagship.cc>
2022-07-22 13:39:51 +0900

    performance improvement but still O(n^2)

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -3,15 +3,13 @@
  * @return {string}
  */
 const longestCommonPrefix = function (strs) {
-  let longest = strs.shift()
-  for (let i = 0; i < strs.length; i++) {
-    let charIndex = 0
-    if (strs[i] === longest) continue
-    while (strs[i][charIndex] === longest[charIndex]) {
-      charIndex++
+  const alphaSort = strs.sort((a, b) => a - b)
+  if (alphaSort[0][0] !== alphaSort[strs.length - 1][0]) return ""
+  const first = alphaSort.shift()
+  for (let i = 0; i < first.length; i++) {
+    for (let j = 0; j < alphaSort.length; j++) {
+      if (first[i] !== alphaSort[j][i]) return first.slice(0, i)
     }
-    if (charIndex === 0) return ""
-    longest = longest.slice(0, charIndex)
   }
-  return longest
+  return first
 }
