9f98cda0ebd1db6f9e3b84452117e8dfba10f23d
pedro <pedro@flagship.cc>
2022-08-02 00:15:02 +0900

    leetcode #200 Number of Islands

---
+++ numIslands.js
@@ -0,0 +1,35 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+const dfs = (grid, i, j) => {
+  if (
+    i < 0 ||
+    i >= grid.length ||
+    j < 0 ||
+    j >= grid[0].length ||
+    grid[i][j] === '0'
+  ) {
+    return
+  }
+
+  grid[i][j] = '0'
+  dfs(grid, i + 1, j)
+  dfs(grid, i - 1, j)
+  dfs(grid, i, j + 1)
+  dfs(grid, i, j - 1)
+}
+
+const numIslands = (grid) => {
+  let count = 0
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === '1') {
+        count += 1
+        dfs(grid, i, j)
+      }
+    }
+  }
+  return count
+}
