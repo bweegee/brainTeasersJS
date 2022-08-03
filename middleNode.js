6fe309cee0313167e99ef499e15cac82e77b615e
Pedro Buigas <pedro@MacBook-Pro-4.local>
2022-08-03 10:11:55 +0900

    O(n) solution

--- middleNode.js
+++ middleNode.js
@@ -23,3 +23,17 @@ const middleNode = (head) => {
   }
   return head
 }
+
+/*
+ * O(n) solution
+ */
+const middleNode = (head) => {
+  let fast = head
+  let slow = head
+
+  while (fast && fast.next) {
+    fast = fast.next.next
+    slow = slow.next
+  }
+  return slow
+}
